f1c5d8b6b578c2b8a7b0c3ad1e1cdc742ffa53c1
Admin <admin@MacBook-Air-de-Admin.fritz.box>
2025-11-09 21:28:03 +0100

    mainly new functions

---
+++ R/nfsa_report_transmissions.R
@@ -0,0 +1,58 @@
+
+
+#' Report transmissions of a table for a period
+#'
+#' @param table_sel the table to select. Options are: "T0801_Q", "T0801SA_Q", "T0800_A"
+#' @param country_sel country or countries for which to find transmissions. By default EU27.
+#' @param period_sel the period to check.
+#' @param output An excel file, or an html file or a chart
+#'
+#' @return an excel file, an html table or a ggplot
+#' @export nfsa_report_transmissions
+#' @import dplyr
+#' @import tidyr
+#' @import ggplot2
+#' @importFrom scales label_date
+#' @importFrom forcats fct_rev
+#' @examples
+#' nfsa_report_transmissions (table_sel = "T0800_A", period_sel = "2024_0000")
+nfsa_report_transmissions <- function(table_sel = "T0801_Q",
+                                      country_sel= nfsa_eu27,
+                                      period_sel,
+                                      path_sel,
+                                      output = "excel"){
+library(dplyr)
+library(ggplot2)
+
+
+
+dat <- map(country_sel, ~nfsa::nfsa_find_xml(country_sel = .x, table_sel = table_sel, period_sel = period_sel,
+  path_sel = path_sel)) |>
+  unlist() |>
+  as_tibble() |>
+  mutate(received = file.mtime(value)) |>
+  tidyr::separate_wider_delim(value,
+                              delim = "_",
+                              names = c("root","SDMX","NASEC", "table", "period",
+                                        "ref_area", "year", "quarter", "version"),
+                              cols_remove = FALSE) |>
+  mutate(version = stringr::str_remove_all(version,".xml")) |>
+  select(file = value, table, ref_area, year, quarter,version,received) |>
+  arrange(ref_area,version)
+
+if (output == "excel") {
+  nfsa_to_excel(dat)
+}
+if (output == "table") {
+  nfsa_to_html(dat)
+}
+
+if (output == "chart") {
+ ggplot(dat,aes(received,forcats::fct_rev(ref_area),colour = version))+
+    geom_point(size = 5)+
+    theme_minimal()+
+    ylab("")+
+    scale_x_datetime(labels = scales::label_date())
+}
+
+}
---
+++ R/nfsa_transmissions.R
@@ -0,0 +1,58 @@
+#' @title Generate NFSA Transmission Report
+#'
+#' @description This function generates a report of NFSA data transmissions received, filtering by a minimum timestamp and outputting the results to an Excel file.
+#'
+#' @param time_min A character string representing the minimum date and time for filtering received files (e.g., "2025-10-20").  Files received before this time will be excluded.
+#' @param output_sel A character string specifying the directory where the output Excel file should be saved. Defaults to `"output/logs"`.
+#' @param recursive A logical value. If `TRUE`, the function will recursively search for files in subdirectories within the specified path. Defaults to `FALSE`.
+#'
+#' @return This function does not return a value, instead outputs an Excel file with the transmission report.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' # Generate a transmission report for files received since "2025-11-01" and save to default location
+#' nfsa_transmissions(time_min = "2025-11-01")
+#'
+#' # Generate a report, save to alternative location and search in subdirectories
+#' nfsa_transmissions(time_min = "2025-10-25", output_sel = "my_output_folder", recursive = TRUE)
+#' }
+#' @export
+nfsa_transmissions <- function(time_min  ,
+                                      output_sel = here("output", "logs"),
+                                      recursive = FALSE){
+  library(tidyverse)
+  library(cli)
+  library(here)
+
+  cli::cli_inform("Looking for files in the server...")
+  received <- list.files(path = "M:/nas/incoming_SDMX_files/",
+                         pattern = "NASEC_",
+                         full.names = TRUE,
+                         recursive = recursive) |>
+    as_tibble()
+
+  cli::cli_inform("Processing info...")
+
+  received <- received |>
+    mutate(received = lubridate::ymd_hms(file.mtime(value))) |>
+    filter(received >= time_min) |>
+    mutate(value = str_remove_all(value,"M:/nas/incoming_SDMX_files/")) |>
+    separate_wider_delim(value,
+                         delim = "_",
+                         names = c("NASEC", "table", "freq", "ref_area","year", "quarter","version"),
+                         cols_remove = FALSE) |>
+    mutate(version = stringr::str_remove_all(version,".xml"),
+           period = paste0(year,"-Q", str_sub(quarter,4,4))) |>
+    select(ref_area,file=value,table, period,version,received)
+
+
+  openxlsx::write.xlsx(received,
+                       file = paste0(output_sel,"/received_",as.character(format(Sys.time(), "%Y%m%d_%H%M%S")),".xlsx"),
+                       overwrite = TRUE,
+                       asTable = TRUE)
+
+  cli::cli_alert_success(paste0("File created in ",output_sel,"/received", as.character(format(Sys.time(), "%Y%m%d_%H%M%S")),".xlsx"))
+
+}
+
